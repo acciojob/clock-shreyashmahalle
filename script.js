974fd32576fd9139ecb9f3bfa2546e7d67141582
shreyashmahalle <128777174+shreyashmahalle@users.noreply.github.com>
2025-10-26 18:11:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function updateTimer(){
+const now new Date();
+document.getElementById("timer").textContent = now.tolocaleString();
+}
+
+setInterval(updateTimer, 1000);
+updateTimer();
